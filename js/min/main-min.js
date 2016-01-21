42d7f3f37fa8554436d3df93596517bca42309b2
JonasBG <jonasbg05@gmail.com>
2016-01-21 07:31:55 -0500

    cambiando fondo y efecto

---
+++ js/min/main-min.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   if (!$('.main-section').hasClass("no-index")) {
+        $('.main-section').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.main-section').css('height', $(window).height());
+        });
+    }
+
+});
+
+
